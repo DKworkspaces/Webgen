887edcb204905028427e8ebf88dcee8d5bc90397
DKworkspaces <divya.kumar.bhagat@gmail.com>
2026-08-21 14:39:53 +0530

    Comment out load_data_profiles call
    Comment out the load_data_profiles() call in generate_layout_snippets function.

--- build.py
+++ build.py
@@ -38,7 +38,7 @@ def load_data_profiles():
 def generate_layout_snippets():
     """Generates raw, ready-to-use HTML blocks inside templates/temp/."""
     md('templates/temp')
-    hd= load_data_profiles()
+    #hd= load_data_profiles()
     # 1. Compile and save plain header snippet
     h_t=base_f('_header.html')
     generate(h_t,'templates/temp/header.html',a=hd)
