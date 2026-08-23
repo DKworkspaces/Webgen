fdd20a1f20dea28bdb3bd2ad839517d6980a0b9a
DKworkspaces <divya.kumar.bhagat@gmail.com>
2026-08-23 11:23:14 +0530

    Update build.py

--- build.py
+++ build.py
@@ -41,10 +41,10 @@ def generate_layout_snippets():
     md('templates/temp')
     hd= load_data_profiles()
     # 1. Compile and save plain header snippet
-    h_t=base_f('marco/_header_base.html')
+    h_t=base_f('_header.html')
     generate(h_t,'templates/temp/header.html',site_config=hd)
     # 2. Compile and save plain footer snippet
-    f_t=base_f('macro/_footer_base.html')
+    f_t=base_f('_footer.html')
     generate(f_t,'templates/temp/footer.html',site_config=hd)
     print("Success: Standalone layout modules compiled into templates/temp/")
         
